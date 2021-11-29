e83ff183c69d9707e0b651bfcdac463e20124dd2
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-29 15:07:08 +0100

    edit line 37 and add script with function

--- jsnack-pariedispari/js/script.js
+++ jsnack-pariedispari/js/script.js
@@ -9,7 +9,7 @@ Dichiariamo chi ha vinto
 In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
 */
 
-
+/*
 //senza funzione
 let user = prompt('Scrivi pari o dispari');
 //controllo se l'utente ha inserito le parole giuste
@@ -34,10 +34,53 @@ console.log('Nome random del PC: ' + numberPc);
 console.log('Somma: ' + sum);
 
 //controllo chi ha vinto
-if (sum % 2 == 0) {
+if (sum % 2 == 0 && user == "pari" || sum % 2 != 0 && user == "dispari") {
     console.log('Ha vinto utente');
 } else {
     console.log ('Ha vinto il PC');
 }
+*/
 
+//con funzione
+//funzione per creare numero random
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+//funzione per vedere se un numero è pari
+function isEven(num) {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let user = prompt('Scrivi pari o dispari');
+//controllo se l'utente ha inserito le parole giuste
+while (user != "pari" && user != "dispari") {
+    user = prompt('Scrivi pari o dispari');
+}
+
+let numberUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+//controllo se l'utente ha inserito il numero giusto
+while (numberUser < 0 || numberUser > 5) {
+    numberUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+}
+
+//creo un numero random e lo sommo con quello dell'utente
+let numberPc = randNum (1, 5);
+let sum = numberUser + numberPc;
+
+//mostro le scelte dell'utente, il numero random e la somma
+console.log('Utente: ' + user);
+console.log('Nome scelto da utente: ' + numberUser);
+console.log('Nome random del PC: ' + numberPc);
+console.log('Somma: ' + sum);
+
+//controllo chi ha vinto
+if (isEven(sum) == true && user == "pari" || isEven(sum) == false && user == "dispari") {
+    console.log('Ha vinto utente');
+} else {
+    console.log('Ha vinto il PC');
+}
