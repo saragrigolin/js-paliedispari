e2b39f4c54e823ae690f16675b52642044a27d28
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-29 14:41:55 +0100

    script method 2 with and without function

--- jsnack-palindroma/js/script.js
+++ jsnack-palindroma/js/script.js
@@ -3,7 +3,8 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 (prima senza funzione e poi con funzione)*/
 
-//senza funzione
+/*
+//senza funzione metodo 1
 let stringa = prompt('Inserisci una parola');
 
 //splitto la stringa in parole e creo un array, sfruttando il metodo reverse
@@ -20,4 +21,47 @@ if (stringa == joinArray) {
     console.log('La parola ' + stringa + ' è palindroma.');
 } else { //altrimenti
     console.log('La parola ' + stringa + ' non è palindroma.');
+}
+*/
+
+/*
+//senza funzione metodo 2
+let string = prompt('Inserisci una parola');
+let reverseString = "";
+for (let i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i];
+}
+
+//mostro la parola al contrario
+console.log('parola scelta: ' + string);
+console.log('parola al contrario: ' + reverseString);
+
+//se sono uguali allora
+if (string == reverseString) {
+    console.log('La parola ' + string + ' è palindroma.');
+} else { //altrimenti
+    console.log('La parola ' + string + ' non è palindroma.');
+}
+*/
+
+//con funzione metodo 2
+function reverseStr(string){
+    let reverseString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string[i];
+    }
+    return reverseString;
+}
+
+let string = prompt('Inserisci una parola');
+
+//mostro la parola al contrario
+console.log('parola scelta: ' + string);
+console.log('parola al contrario: ' + reverseStr(string));
+
+//se sono uguali allora
+if (string == reverseStr(string)) {
+    console.log('La parola ' + string + ' è palindroma.');
+} else { //altrimenti
+    console.log('La parola ' + string + ' non è palindroma.');
 }
