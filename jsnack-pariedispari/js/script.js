3d79fa427a482837ce77d91806c9b3b1b4d52986
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-29 14:44:34 +0100

    init

---
+++ jsnack-pariedispari/js/script.js
@@ -0,0 +1,11 @@
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari
+Dichiariamo chi ha vinto
+(corrispondenza tra scelta pari e dispari e somma pari o dispari)
+In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
+*/
+
